d80da46b5b2cd0cf2da869254598a46c688965e4
anyelicardenas <anyee.li16@gmail.com>
2024-07-23 11:01:12 -0500

    118. Pascal's Triangle

--- 118.PascalsTriangle.py
+++ 118.PascalsTriangle.py
@@ -7,18 +7,18 @@ class Solution:
             return [[1]]
         elif numRows == 2:
             return [[1], [1,1]]
-        elif numRows == 3:
-            return resultado
         
-        prueba = [1, 1]
-        iterar = [1, 2, 1]
+        esquinas = [1, 1]
+        piso_tres_triangulo = [1, 2, 1]
         while len(resultado) != numRows:
-            for actual, siguiente in zip(iterar, iterar[1:]):
+            if numRows == 3:
+                return resultado
+            for actual, siguiente in zip(piso_tres_triangulo, piso_tres_triangulo[1:]):
                 suma = actual + siguiente
-                prueba.insert(1, suma)
-                if len(prueba) > len(iterar):
-                    resultado.append(prueba)
-                    iterar = prueba
-                    prueba = [1,1]
+                esquinas.insert(1, suma)
+                if len(esquinas) > len(piso_tres_triangulo):
+                    resultado.append(esquinas)
+                    piso_tres_triangulo = esquinas
+                    esquinas = [1,1]
             
         return resultado
