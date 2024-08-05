5712c705eae9f4ee3b7cb6c7af5fa1a85c74cb92
anyelicardenas <anyee.li16@gmail.com>
2024-08-04 19:06:54 -0500

    455. Assign Cookies

---
+++ 455.AssignCookies.py
@@ -0,0 +1,11 @@
+# url exercise:https://leetcode.com/problems/assign-cookies/
+class Solution:
+    def findContentChildren(self, g: List[int], s: List[int]) -> int:
+        g.sort()
+        s.sort()
+        i = j = 0
+        while i < len(g) and j < len(s):
+            if s[j] >= g[i]:
+                i +=1
+            j += 1
+        return i
