a2c15d4db7068d74e0a24f7a3f6a4ecfa1d2bc18
anyeli cardenas <anyee.li16@gmail.com>
2025-01-10 15:38:46 -0500

    Premium unlocked exercises in leetcode

---
+++ Premium_leetcode_exercises/SQL/512.GamePlayAnalysisII.sql
@@ -0,0 +1,56 @@
+-- 512. Game Play Analysis II
+"""Table: Activity
+
++--------------+---------+
+| Column Name  | Type    |
++--------------+---------+
+| player_id    | int     |
+| device_id    | int     |
+| event_date   | date    |
+| games_played | int     |
++--------------+---------+
+(player_id, event_date) is the primary key (combination of columns with unique values) of this table.
+This table shows the activity of players of some games.
+Each row is a record of a player who logged in and played a number of games (possibly 0) before logging out on someday using some device.
+ 
+
+Write a solution to report the device that is first logged in for each player.
+
+Return the result table in any order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Activity table:
++-----------+-----------+------------+--------------+
+| player_id | device_id | event_date | games_played |
++-----------+-----------+------------+--------------+
+| 1         | 2         | 2016-03-01 | 5            |
+| 1         | 2         | 2016-05-02 | 6            |
+| 2         | 3         | 2017-06-25 | 1            |
+| 3         | 1         | 2016-03-02 | 0            |
+| 3         | 4         | 2018-07-03 | 5            |
++-----------+-----------+------------+--------------+
+Output: 
++-----------+-----------+
+| player_id | device_id |
++-----------+-----------+
+| 1         | 2         |
+| 2         | 3         |
+| 3         | 1         |
++-----------+-----------+"""
+# Write your MySQL query statement below
+WITH find_first_log AS (SELECT player_id, device_id, event_date, games_played, 
+MIN(event_date) OVER(PARTITION BY player_id) AS min_date
+FROM Activity
+ORDER BY  player_id
+)
+
+SELECT 
+player_id, device_id
+FROM find_first_log
+WHERE event_date = min_date
