0f21c326b45750d6a8f446f49f360c978667df4c
anyeli cardenas <anyee.li16@gmail.com>
2025-01-09 19:03:02 -0500

    Premium unlocked exercises in leetcode

---
+++ Premium_leetcode_exercises/SQL/1112.HighestGradeForEachStudent.sql
@@ -0,0 +1,43 @@
+"""Write a solution to find the highest grade with its corresponding course for each student. In case of a tie, you should find the course with the smallest course_id.
+
+Return the result table ordered by student_id in ascending order.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Enrollments table:
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 2          | 2         | 95    |
+| 2          | 3         | 95    |
+| 1          | 1         | 90    |
+| 1          | 2         | 99    |
+| 3          | 1         | 80    |
+| 3          | 2         | 75    |
+| 3          | 3         | 82    |
++------------+-----------+-------+
+Output: 
++------------+-------------------+
+| student_id | course_id | grade |
++------------+-----------+-------+
+| 1          | 2         | 99    |
+| 2          | 2         | 95    |
+| 3          | 3         | 82    |
++------------+-----------+-------+"""
+
+WITH find_highest_grade AS (SELECT student_id, course_id, grade,
+MAX(grade) OVER(PARTITION BY student_id) as highest_grade
+FROM Enrollments
+ORDER BY course_id ASC
+)
+
+SELECT student_id, course_id, grade
+FROM find_highest_grade
+WHERE grade = highest_grade
+GROUP BY student_id, grade
+ORDER BY student_id
